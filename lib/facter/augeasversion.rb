538cba835ddf34728baa4e5fa1b24ecf934e9e59
Alan Shields <alan@alan.shields.name>
2011-05-12 15:33:09 -0700

    Some versions of libaugeas don't seem to have a NO_LOAD, so use NONE

--- lib/facter/augeasversion.rb
+++ lib/facter/augeasversion.rb
@@ -3,9 +3,15 @@ Facter.add("augeasversion") do
 
     begin
       require 'augeas'
-      aug = Augeas::open('', '', Augeas::NO_LOAD & Augeas::NO_STDINC)
-      aug.get('/augeas/version') || nil
+      if Augeas.const_defined?("NO_LOAD") && Augeas.const_defined?("NO_STDINC")
+        aug = Augeas::open('', '', Augeas::NO_LOAD & Augeas::NO_STDINC)
+      elsif Augeas.const_defined?("NONE")
+        aug = Augeas::open('', '', Augeas::NONE)
+      else
+        nil
+      end
 
+      aug.get('/augeas/version') || nil
     rescue LoadError # don't fail if augeas isn't installed
       nil
     end
